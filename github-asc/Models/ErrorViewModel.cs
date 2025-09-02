70f316468fee110ea617f529565b65642eafc864
Thejas <Thejas@manishkapoor2606gmail.onmicrosoft.com>
2025-09-02 09:32:41 +0530

    Add project files.

---
+++ github-asc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace github_asc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
